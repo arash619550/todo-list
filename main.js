ec065d6557c24eff5864f5666970236eb8024e90
arash619550 <arash619551@gmail.com>
2025-05-08 11:22:59 +0330

    complete

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 const ul = document.querySelector(".task-list");
-const recycleBinLogo = document.createElement("img");
-recycleBinLogo.src = "recycle bin.webp";
-recycleBinLogo.width = 25;
+const dish = [];
 button.addEventListener("click", function (event) {
     event.preventDefault();
     if (input.value !== "") {
         const li = document.createElement("li");
         li.innerText = input.value;
         li.classList.add("style");
-        li.addEventListener("click", function () {
+        const recycleBinLogo = document.createElement("img");
+        recycleBinLogo.src = "recycle bin.webp";
+        recycleBinLogo.width = 25;
+        recycleBinLogo.classList.add("recycle-img");
+        li.appendChild(recycleBinLogo);
+        recycleBinLogo.addEventListener("click", function () {
             ul.removeChild(li);
         });
         ul.appendChild(li);
-        ul.appendChild(recycleBinLogo);
-        recycleBin();
         input.value = "";
+        dish.push(li.innerText);
+        console.log(dish);
     }
 });
-
-
-function recycleBin() {
-
-}
