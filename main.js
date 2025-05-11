1b14c2c2467e3678227b5118120df4c48bebba6f
arash619550 <arash619551@gmail.com>
2025-05-11 16:45:51 +0330

    add obj

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 const ul = document.querySelector(".task-list");
-const dish = [];
+const tasks = [];
+
 button.addEventListener("click", function (event) {
     event.preventDefault();
     if (input.value !== "") {
+        const task = { task: input.value, done: false };
+        tasks.push(task);
         const li = document.createElement("li");
         li.innerText = input.value;
         li.classList.add("style");
         const recycleBinLogo = document.createElement("img");
-        const tick = document.createElement("img");
         recycleBinLogo.src = "assets/images/recycle bin.webp";
-        tick.src = "assets/images/tick.webp";
-        tick.width = 25;
         recycleBinLogo.width = 25;
         recycleBinLogo.classList.add("recycle-tick-img");
-        recycleBinLogo.classList.add("recycle-tick-img");
+        const tick = document.createElement("img");
+        tick.src = "assets/images/tick.webp";
+        tick.width = 25;
+        tick.classList.add("recycle-tick-img");
         li.append(recycleBinLogo, tick);
+        ul.appendChild(li);
         recycleBinLogo.addEventListener("click", function () {
             ul.removeChild(li);
+            const index = tasks.indexOf(task);
+            if (index !== -1) {
+                tasks.splice(index, 1);
+            }
+            console.log(tasks);
         });
         tick.addEventListener("click", function () {
             li.style.color = "green";
             li.style.textDecoration = "line-through";
+            task.done = true;
         });
-        ul.appendChild(li);
         input.value = "";
-        dish.push(li.innerText);
-        console.log(dish);
     }
 });
