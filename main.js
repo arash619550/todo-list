7b7a5597455e6601c053c634926e5848d7866dfe
arash619550 <arash619551@gmail.com>
2025-05-16 16:22:37 +0330

    complete

--- main.js
+++ main.js
@@ -5,33 +5,31 @@ let tasks = [];
 
 button.addEventListener("click", event => {
     event.preventDefault();
-    if (input.value != "") {
-        const objTask = { task: input.value, done: false };
+    if (input.value.trim() !== "") {
+        const objTask = { task: input.value.trim(), done: false };
         tasks.push(objTask);
         input.value = "";
         render();
     }
-})
+});
 
 const render = () => {
-    ul.innerHTML = tasks.map(function (item, index) {
-        return `<li class="style ${item.done ? "done" : ""}" dir="rtl" data-index=${index}>
-            <img src="assets/images/tick.webp" width="25px" onclick="tickFunction(${index})">
+    ul.innerHTML = tasks.map((item, index) => {
+        return `
+        <li class="style ${item.done ? "done" : ""}" dir="rtl" data-index="${index}">
+            <img src="assets/images/${item.done ? "trash.png" : "tick.webp"}" width="25px" onclick="tickFunction(this, ${index})">
             <img src="assets/images/recycle bin.webp" width="25px" onclick="deleteFunction(${index})">
             ${item.task}
         </li>`;
     }).join("");
-}
+};
 
-
-function tickFunction(index) {
-    tasks[index].done = !tasks[index].done;;
-    const li = ul.querySelector(`li[data-index="${index}"]`);
+function tickFunction(img, index) {
+    tasks[index].done = !tasks[index].done;
     render();
 }
 
 const deleteFunction = index => {
     tasks.splice(index, 1);
     render();
-}
-
+};
